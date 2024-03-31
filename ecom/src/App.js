ae0258857f6934e9b7afe0d3c18f8dac0ebb7c84
k suraj <surajsuraj87850@gmail.com>
2024-03-31 10:07:13 +0530

    first commit

---
+++ ecom/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-8xl">
+      <h1>welcome to Ecomerce project</h1>
+    </div>
+  );
+}
+
+export default App;
